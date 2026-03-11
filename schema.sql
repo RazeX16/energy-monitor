f14073c36e5b92b494b8987fdbdaa6a0f69c9687
dileep <dileep@ippaimail.org>
2026-03-11 15:33:42 +0530

    Add PostgreSQL schema

---
+++ schema.sql
@@ -0,0 +1,41 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict V3OrAH1o959ObrWdwI2Ve8oFsjB6LmMnc9INIOxN0xbBQNYBm3ZoahAVrjCZqhe
+
+-- Dumped from database version 18.3
+-- Dumped by pg_dump version 18.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: timescaledb; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS timescaledb WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION timescaledb; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION timescaledb IS 'Enables scalable inserts and complex queries for time-series data (Community Edition)';
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict V3OrAH1o959ObrWdwI2Ve8oFsjB6LmMnc9INIOxN0xbBQNYBm3ZoahAVrjCZqhe
+
